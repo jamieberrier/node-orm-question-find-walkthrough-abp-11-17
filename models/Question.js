ceadda9931f21120b7f772352a546101bdd78b08
jamieberrier <berrier.jamie@gmail.com>
2017-12-04 19:30:00 +0000

    automatically backed up by learn

--- models/Question.js
+++ models/Question.js
@@ -30,9 +30,13 @@ class Question{
   }
 
   static Find(id){
+    const sql = "SELECT * FROM questions WHERE id = ?"
     return new Promise(function(resolve){
-      const question = new Question()
-      resolve(question)
+      db.get(sql, [id], function(err, result){
+        const question = new Question(result.content)
+        question.id = result.id
+        resolve(question)
+      })
     })
   }
 }
